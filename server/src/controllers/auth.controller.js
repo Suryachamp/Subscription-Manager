37d70e9f1c36d525fd141cbda73f692d9f70fa84
Surya Sekhar Prajapati <surya2004sekhar@gmail.com>
2026-06-20 23:53:11 +0530

    starting to add controller for register api

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,5 +1,18 @@
-exports.register = async (req, res) => {
-    res.status(200).json({
-        message: "user registered successfully"
-    });
+const prisma = require('@prisma/client')
+const bcrypt = require("bcrypt")
+
+exports.register = async (req,res)=>{
+    try{
+        const {name,email,password}=req.body;
+
+        //validates request body
+        if(!name || !email || !password){
+            return res.status(400).json({
+                message:"All fields are required",
+            });
+        }
+
+        //check if the user is already existing or not
+        
+    }
 }
