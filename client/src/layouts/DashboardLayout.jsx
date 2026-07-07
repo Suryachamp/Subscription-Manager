a0458e68e2909eb874f1fe5e9b47ab865e9e0646
Surya Sekhar Prajapati <surya2004sekhar@gmail.com>
2026-07-07 23:49:32 +0530

    created dashboard layout

--- client/src/layouts/DashboardLayout.jsx
+++ client/src/layouts/DashboardLayout.jsx
@@ -1,16 +1,33 @@
-import { Sidebar } from "lucide-react";
-import Navbar from "../components/layout/Navbar";
 import { Outlet } from "react-router-dom";
 
 function DashboardLayout() {
     return (
-        <div>
-            <Navbar/>
-            <Sidebar/>
+        <div className="min-h-screen bg-[#F7F5F2]">
 
-            <main>
-                <Outlet/>gi 
-            </main>
+            {/* Sidebar */}
+
+            <aside className="w-[280px] min-h-screen border-r border-gray-200">
+                Sidebar
+            </aside>
+
+            {/* Main */}
+
+            <div className="flex-1">
+
+                {/* Navbar */}
+
+                <header className="h-20 border-b border-gray-200">
+                    Navbar
+                </header>
+
+                {/* Page */}
+
+                <main className="p-8">
+                    <Outlet/>
+                </main>
+
+            </div>
+
         </div>
     );
 }
